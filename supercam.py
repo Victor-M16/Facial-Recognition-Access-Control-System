dfa9bdc12603e893afd0820185cf24e05aa3e99d
Victor Mjimapemba <vcmjimapemba@gmail.com>
2024-04-25 15:26:23 +0200

    update

--- supercam.py
+++ supercam.py
@@ -5,38 +5,95 @@ import time
 import pickle
 import face_recognition
 from flask_socketio import SocketIO, emit
+import requests
+import json
+import time
+
+ESP32_IP = "192.168.8.173"  # Replace with the IP address of your ESP32
+def get_lock_status():
+    url = f"http://{ESP32_IP}:80/lock-status"  # Updated endpoint URL
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print("Successfully linked ESP32")
+        return data["status"]
+    else:
+        print("Failed to get lock status")
+        return None
+
+def lock():
+    url = f"http://{ESP32_IP}:80/lock"  # Updated endpoint URL
+    response = requests.post(url)
+    if response.status_code == 200:
+        print("Lock command sent successfully")
+    else:
+        print("Failed to send lock command")
+
+def unlock():
+    url = f"http://{ESP32_IP}:80/unlock"  # Updated endpoint URL
+    response = requests.post(url)
+    if response.status_code == 200:
+        print("Unlock command sent successfully")
+    else:
+        print("Failed to send unlock command")
+
+
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+
+
+
+# Determine faces from encodings.pickle file model created from train_model.py
+encodingsP = "/home/mjima/facial_recognition/supercam/superstream/encodings.pickle"
+
+# Load the known faces and embeddings along with OpenCV's Haar
+# cascade for face detection
+print("[INFO] loading encodings + face detector...")
+data = pickle.loads(open(encodingsP, "rb").read())
+
+# Dictionary to store known faces
+known_faces = {}
+
+# Loop through the encodings and extract unique faces
+for i, encoding in enumerate(data["encodings"]):
+    name = data["names"][i]
+    if name not in known_faces.values():
+        known_faces[i] = name
+
+known_faces_json = json.dumps(known_faces)
+
+
+# Default namespace handlers
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected to websocket')
 
 @socketio.on('message')
 def handle_message(message):
     print('Received message:', message)
     socketio.emit('message', message)
 
-# Handle WebRTC signaling messages
-@socketio.on('offer')
-def handle_offer(offer):
-    emit('answer', offer, broadcast=True)
-
-@socketio.on('candidate')
-def handle_candidate(candidate):
-    emit('candidate', candidate, broadcast=True)
+# '/faces' namespace handlers
+@socketio.on('connect', namespace="/faces")
+def handle_connect_faces():
+    print('Client connected to faces websocket') 
+    socketio.emit('message', known_faces_json, namespace='/faces')
 
 
-# Determine faces from encodings.pickle file model created from train_model.py
-encodingsP = "/home/mjima/facial_recognition/supercam/superstream/encodings.pickle"
-known_faces = {1:"Victor",
-               2:"Pemphero",
-               3:"Roberta",
-               4:"Cliff"}
+@socketio.on('message', namespace='/faces')
+def send_message_faces(message):
+    print('Sent:', message)
+    # Broadcast the message to all clients in the '/faces' namespace
+    socketio.emit('message', message, namespace='/faces')
 
 
-# Load the known faces and embeddings along with OpenCV's Haar
-# cascade for face detection
-print("[INFO] loading encodings + face detector...")
-data = pickle.loads(open(encodingsP, "rb").read())
+print(known_faces)
+print(known_faces_json)
+
+
+
 
 # Singleton pattern to ensure only one camera instance
 class VideoCameraSingleton:
@@ -76,7 +133,7 @@ class VideoCamera:
 def recognize_faces(camera):
     
     print("face recon function working")
-    currentname = "unknown"
+    currentname = "Unknown "
     while True:
         frame = camera.get_frame()
         if frame is None:
@@ -91,14 +148,16 @@ def recognize_faces(camera):
                 matchedIdxs = [i for (i, b) in enumerate(matches) if b]
                 counts = {}
                 for i in matchedIdxs:
-                    name = data["names"][i]
+                    name = data["names"][i] #change name to the matched one
                     counts[name] = counts.get(name, 0) + 1
                 name = max(counts, key=counts.get)
                 if currentname != name:
                     currentname = name
-                    # Need a way to interact with the WebSocket here so that the client receives the newly detected current_name only when it is different from the one detected before
                     print(currentname)
                     handle_message(currentname)
+                    if currentname in known_faces.values():
+                        unlock()
+                    
             names.append(name)
 
 def gen(camera):
@@ -116,6 +175,8 @@ def gen(camera):
 
 @app.route('/')
 def index():
+    lock()
+    get_lock_status()
     return render_template('index2.html')
 
 def video_feed():
@@ -135,81 +196,3 @@ if __name__ == '__main__':
 
 
 
-
-# import cv2
-# import numpy as np
-# from flask import Flask, render_template, Response
-# from flask_socketio import SocketIO, emit
-# import base64
-# import threading
-# import time
-# import pickle
-# import face_recognition
-
-# app = Flask(__name__)
-# socketio = SocketIO(app)
-
-# # Load the known faces and embeddings
-# encodingsP = "encodings.pickle"
-# data = pickle.loads(open(encodingsP, "rb").read())
-
-# # Video streaming function with face recognition
-# def gen():
-#     # Open the camera
-#     cap = cv2.VideoCapture(0)
-
-#     while True:
-#         # Capture frame-by-frame
-#         ret, frame = cap.read()
-
-#         # Perform face recognition
-#         boxes = face_recognition.face_locations(frame)
-#         encodings = face_recognition.face_encodings(frame, boxes)
-
-#         # Loop through the detected faces
-#         for encoding in encodings:
-#             matches = face_recognition.compare_faces(data["encodings"], encoding)
-#             name = "Unknown"
-
-#             # Check if a match is found
-#             if True in matches:
-#                 matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-#                 counts = {}
-
-#                 # Determine the recognized face with the most votes
-#                 for i in matchedIdxs:
-#                     name = data["names"][i]
-#                     counts[name] = counts.get(name, 0) + 1
-#                 name = max(counts, key=counts.get)
-
-#                 # Draw a rectangle and label around the face
-#                 y1, x2, y2, x1 = boxes[0]
-#                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-#                 cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
-
-#         # Convert frame to base64 string
-#         ret, buffer = cv2.imencode('.jpg', frame)
-#         frame_base64 = base64.b64encode(buffer)
-
-#         # Send the frame to the client
-#         socketio.emit('video_stream', {'image': frame_base64.decode('utf-8')})
-
-#         # Break the loop if 'q' is pressed
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             break
-
-#     # Release the camera
-#     cap.release()
-
-# @app.route('/')
-# def index():
-#     return render_template('index2.html')
-
-# @socketio.on('connect')
-# def connect():
-#     print('Client connected')
-#     emit('connected', {'data': 'Connected'})
-
-# if __name__ == '__main__':
-#     threading.Thread(target=gen).start()
-#     socketio.run(app, host='0.0.0.0', port=8000)
